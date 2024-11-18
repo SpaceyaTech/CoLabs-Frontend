6e550dfc2d6889a42ef63ca3b70f87ff7429e9e4
bridgitkanini <bridgit.k.mutinda@gmail.com>
2024-11-18 13:05:51 +0300

    Added font cofiguration

--- src/routes/-components/HomePage.tsx
+++ src/routes/-components/HomePage.tsx
@@ -5,7 +5,7 @@ export function HomePage() {
   return (
     <div className="justify-center flex h-full min-h-screen w-full flex-col items-center">
         <LandingPageNavbar />
-      <div className="justify-center gap-3 flex min-h-screen h-full w-full flex-col items-center">
+      <div className="font-ff-poppins justify-center gap-3 flex min-h-screen h-full w-full flex-col items-center">
         {/* landing page goes here  */}
         <p className="text-3xl  border border-primary rounded-2xl p-5">
           Landing page goes here
