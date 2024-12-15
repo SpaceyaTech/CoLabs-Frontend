1ffa8c18451ebad1412e05b2045aff0973394cea
tigawanna <denniskinuthiawaweru@gmail.com>
2024-12-15 18:03:43 +0300

    feat: add scroll restoration

--- src/routes/-components/RootComponent.tsx
+++ src/routes/-components/RootComponent.tsx
@@ -7,12 +7,12 @@ import { TanStackRouterDevtools } from "./RouterDevttols";
 export function RootComponent() {
   return (
     <div className="content min-h-screen w-full bg-gradient-to-r from-base-100 via-primary/20 to-base-100 dark:via-primary/20">
-      <ScrollRestoration />
-      <Outlet />
+      <ScrollRestoration/>
+      <Outlet/>
       <ReactQueryDevtools buttonPosition="bottom-right" />
       <TanStackRouterDevtools position="bottom-left" />
       <Toaster reverseOrder />
-      <TailwindIndicator />
+      <TailwindIndicator/>
     </div>
   );
 }
