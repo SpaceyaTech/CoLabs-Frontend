50901a0bd87ffdc92567e2304f7e7bb72bc24d00
tigawanna <denniskinuthiawaweru@gmail.com>
2024-12-15 18:03:08 +0300

    add scrill restoration

--- src/routes/-components/RootComponent.tsx
+++ src/routes/-components/RootComponent.tsx
@@ -1,17 +1,18 @@
 import { TailwindIndicator } from "@/components/navigation/tailwind-indicator";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Outlet } from "@tanstack/react-router";
+import { Outlet, ScrollRestoration } from "@tanstack/react-router";
 import { Toaster } from "react-hot-toast";
 import { TanStackRouterDevtools } from "./RouterDevttols";
 
 export function RootComponent() {
   return (
-    <div className="content min-h-screen bg-gradient-to-r from-base-100 via-primary/20 dark:via-primary/20 to-base-100 w-full">
+    <div className="content min-h-screen w-full bg-gradient-to-r from-base-100 via-primary/20 to-base-100 dark:via-primary/20">
+      <ScrollRestoration />
       <Outlet />
       <ReactQueryDevtools buttonPosition="bottom-right" />
       <TanStackRouterDevtools position="bottom-left" />
       <Toaster reverseOrder />
-      <TailwindIndicator/>
+      <TailwindIndicator />
     </div>
   );
 }
